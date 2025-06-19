d75e0ed98930daf7c08938fa0b1619e52ffae74e
drizzleL <lxy1990@gmail.com>
2025-06-19 09:14:21 +0800

    up

--- 2518.go
+++ 2518.go
@@ -1,27 +1,28 @@
 package main
 
-import "log"
-
 func countPartitions2(nums []int, k int) int {
 	ret := fastPow(2, len(nums), 1)
-	ret -= 1
 	if ret < 0 {
 		ret += 1e9 + 7
 	}
 	dp := make([]int, k)
 	dp[0] = 1
+	var total int
 	for i := 0; i < len(nums); i++ {
+		total += nums[i]
 		for j := k - 1; j >= nums[i]; j-- {
 			dp[j] += dp[j-nums[i]]
 			dp[j] %= 1e9 + 7
 		}
 	}
+	if total < k*2 {
+		return 0
+	}
 	var sum int
-	for i := 1; i < k; i++ {
+	for i := 0; i < k; i++ {
 		sum += dp[i]
 		sum %= 1e9 + 7
 	}
-	log.Println(ret, sum)
 	sum *= 2
 	sum %= 1e9 + 7
 	ret -= sum
